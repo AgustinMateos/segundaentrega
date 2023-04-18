272b2c3fa707bb07aeb86c5c8326698b0bcd93db
agustin mateos <agustinrotondomateos@gmail.com>
2023-04-17 22:32:19 -0300

    products

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -35,24 +35,6 @@ export const getProductsCart = async (req, res) => {
         })
     }
 }
-/*
-export const getProduct = async (req, res) => {
-    const { id } = req.params
-    try {
-        const product = await managerCart.getElementById(id);
-        if (product) {
-            return res.status(200).json(product)
-        }
-        res.status(200).json({
-            message: "Producto no encontrado"
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-}
-*/
 export const addProductCart = async (req, res) => {
     const { id } = req.params
     const { id_prod, cant } = req.body
@@ -66,41 +48,3 @@ export const addProductCart = async (req, res) => {
         })
     }
 }
-/*
-export const updateProduct = async (req, res) => {
-    const { id } = req.params
-    const { title, description, code, price, status, stock, category, thumbnails } = req.body
-    try {
-        const product = await managerCart.updateElement(id, { title: title, description: description, code: code, price: price, status: status, stock: stock, category: category, thumbnails: thumbnails })
-        if (product) {
-            return res.status(200).json({
-                message: "Producto actualizado"
-            })
-        }
-        res.status(200).json({
-            message: "Producto no encontrado"
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-}
-export const deleteProduct = async (req, res) => {
-    const { id } = req.params
-    try {
-        const product = await managerCart.deleteElement(id)
-        if (product) {
-            return res.status(200).json({
-                message: "Producto eliminado"
-            })
-        }
-        res.status(200).json({
-            message: "Producto no encontrado"
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-}*/
